2f25fdeab0178093ed4de9d1eb557c75a1a34a8f
Namrata <namrata@Namratas-MacBook-Air.local>
2016-09-21 14:25:35 +0530

    Initial Commit

---
+++ NamNavigation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NamNavigation
+//
+//  Created by Namrata on 21/09/16.
+//  Copyright © 2016 Namrata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NamNavigationTests/NamNavigationTests.m
@@ -0,0 +1,39 @@
+//
+//  NamNavigationTests.m
+//  NamNavigationTests
+//
+//  Created by Namrata on 21/09/16.
+//  Copyright © 2016 Namrata. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NamNavigationTests : XCTestCase
+
+@end
+
+@implementation NamNavigationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
